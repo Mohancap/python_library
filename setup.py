eea45ef371e26bcb086faa4c290cb5fac34b30c7
Tirupati <murali.tirupati@capgemini.com>
2022-05-13 18:44:47 +0530

    added library folder

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+ 
+setuptools.setup(
+name='arthcalculator',
+version='0.1',
+description='Basic arthmetics calculator',
+url='',  
+author='Murali Mohan',
+author_email='',
+license='MIT', 
+keywords='calculator', 
+packages=setuptools.find_packages(),
+install_requires=[''],
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+],
+)
